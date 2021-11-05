07f1f130d7da1ed2c13f88efad6325f884dcf23a
sreeliptaa <sreelipta65@gmail.com>
2021-11-05 21:29:14 +0530

    Added: Code and testcase for calculate minimum fare

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -10,15 +10,19 @@ package com.bridgelabz;
 public class InvoiceGenerator {
     private static final int COST_PER_TIME = 1;
     private static final double MINIMUM_COST_PER_KILOMETER = 10.0;
+    private static final double MINIMUM_FARE = 5.0;
 
     /**
-     * Purpose : To calculate the total fare
+     * Purpose : To calculate the minimum fare
      *
      * @param distance : this is the first parameter
      * @param time     : this is the second parameter
-     * @return the total fare
+     * @return the minimum fare
      */
     public double calculateFare(double distance, int time) {
-        return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        if (totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalFare;
     }
 }
--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -12,4 +12,13 @@ public class InvoiceServiceTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assertions.assertEquals(25, fare, 0.0);
     }
+
+    @Test
+    void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(5, fare, 0.0);
+    }
 }
